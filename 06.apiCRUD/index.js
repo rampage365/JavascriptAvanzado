3a24797b0a8d640572912d65d385f3bc0f2e379f
Jesua Luján <code.masterqro@gmail.com>
2022-03-22 20:44:42 -0600

    ejecutamos en el index principal createAuthor

--- 06.apiCRUD/index.js
+++ 06.apiCRUD/index.js
@@ -5,4 +5,17 @@ const goodReadsCrud = require ('./crudAuthors.js')
 // goodReadsCrud.listAuthors()
 
 //Ver un Author
-goodReadsCrud.getAuthor(14567)
+// goodReadsCrud.getAuthor(14712) //14567 , //17060
+
+// CREAR UN AUTOR 
+const jsonSend = {
+    name: "Juan",
+	last_name: "Ramirez",
+	nacionalidad: "MX",
+	biography: "Escritor",
+	gender: "M",
+	age: 30,
+	is_alive: true
+}
+
+goodReadsCrud.createAuthor(jsonSend)
